ac1aff1735d49a1bf73296f3b9978bf83c983213
Moeez Iqbal <moeeziqbalmughal@gmail.com>
2024-06-25 15:03:26 +0500

    Changes

--- Frontend/src/sideBarComponents/eMailComponents/getTemplateEmail.jsx
+++ Frontend/src/sideBarComponents/eMailComponents/getTemplateEmail.jsx
@@ -9,7 +9,7 @@ const GetTemplateEmail = () => {
     useEffect(() => {
         const fetchTemplates = async () => {
             try {
-                const response = await axios.get('http://localhost:3000/gettempelate'); // Corrected endpoint URL
+                const response = await axios.get('http://localhost:3000/gettemplates'); // Corrected endpoint URL
                 setEmails(response.data);
             } catch (error) {
                 console.error('Error fetching templates:', error);
