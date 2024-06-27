aa8e4eef3b3cf7b142b55525b0b6a298c32d71b0
usama <im.uxama85@gmail.com>
2024-06-27 18:30:34 +0500

    Changes

--- Backend/src/controller/EmailList/index.js
+++ Backend/src/controller/EmailList/index.js
@@ -16,41 +16,50 @@ export const createEmailList = async (req, res) => {
 
 
 export const getEmailLists = async (req, res) => {
-    try {
-      // Extracting filter parameters from request query
-      const {
-        Listname,
-        industry, country, city, name, timezone,
-        totalYearsOfExperience, timeInCurrentRole,
-        sequence, lastActivity, emailSent,
-        emailClicked, emailReplied, emailMeetingSet, emailBounced
-      } = req.query;
+  try {
+    // Extracting filter parameters from request query
+    const {
+      Listname, industry, country, city, name, timezone,
+      totalYearsOfExperience, timeInCurrentRole, sequence,
+      lastActivity, company, emailSent, emailClicked,
+      emailReplied, emailMeetingSet, emailBounced
+    } = req.query;
 
+    // Constructing filter object based on provided parameters
+    const filter = {};
+    if (Listname) filter.Listname = Listname;
+    if (industry) filter.industry = industry;
+    if (country) filter.country = country;
+    if (city) filter.city = city;
+    if (name) filter.name = name;
+    if (timezone) filter.timezone = timezone;
+    if (totalYearsOfExperience) filter.totalYearsOfExperience = totalYearsOfExperience;
+    if (timeInCurrentRole) filter.timeInCurrentRole = timeInCurrentRole;
+    if (sequence) filter.sequence = sequence;
+    if (lastActivity) filter.lastActivity = new Date(lastActivity);
+    if (company) filter.company = company;
+    if (emailSent) filter.emailSent = emailSent === 'true';
+    if (emailClicked) filter.emailClicked = emailClicked === 'true';
+    if (emailReplied) filter.emailReplied = emailReplied === 'true';
+    if (emailMeetingSet) filter.emailMeetingSet = emailMeetingSet === 'true';
+    if (emailBounced) filter.emailBounced = emailBounced === 'true';
 
-      // Constructing filter object based on provided parameters
-      const filter = {};
-      if(Listname) filter.Listname = Listname
-      if (industry) filter.industry = industry;
-      if (country) filter.country = country;
-      if (city) filter.city = city;
-      if (name) filter.name = name; 
-      if (timezone) filter.timezone = timezone;
-      if (totalYearsOfExperience) filter.totalYearsOfExperience = totalYearsOfExperience;
-      if (timeInCurrentRole) filter.timeInCurrentRole = timeInCurrentRole;
-      if (sequence) filter.sequence = sequence;
-      if (lastActivity) filter.lastActivity = new Date(lastActivity);  
-      if (emailSent) filter.emailSent = emailSent === 'true';  
-      if (emailReplied) filter.emailReplied = emailReplied === 'true';  
-      if (emailMeetingSet) filter.emailMeetingSet = emailMeetingSet === 'true';  
-      if (emailBounced) filter.emailBounced = emailBounced === 'true';  
-  
-      // Retrieving email lists based on the constructed filter
-      const emailLists = await EmailList.find(filter);
-  
-      res.json(emailLists);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send('Server error');
-    }
-  };
-  
+    // Retrieving email lists based on the constructed filter
+    const emailLists = await EmailList.find(filter);
+
+    res.json(emailLists);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+export const getUniqueCompanies = async (req, res) => {
+  try {
+    const companies = await EmailList.distinct('company');
+    res.json(companies);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
